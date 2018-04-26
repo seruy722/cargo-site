9f49806ebf592333f38a64b9652154af8b0775df
Sergey <seruy722@gmail.com>
2018-04-26 11:25:47 +0300

    add debtResource

--- cargo.site/app/Debt.php
+++ cargo.site/app/Debt.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Debt extends Model
 {
     protected $fillabe = ['number_bag', 'client_id', 'count_place', 'kg', 'shop_name', 'inventory', 'count_sings', 'code_place', 'barnd', 'fax_name', 'notation'];
-    public function clients()
+
+    public function client()
     {
         return $this->belongsTo(Client::class);
     }
